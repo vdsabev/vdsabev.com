18b4a911c53119de8fbcc76f55fe1d412de8b363
Vladimir Sabev <vdsabev@gmail.com>
2017-10-10 20:44:06 +0300

    Fixed history popstate

--- src/router.js
+++ src/router.js
@@ -8,37 +8,42 @@ export const Routes = {
   POSTS: { path: '/posts', title: 'Posts' }
 };
 
-const initialRouteKey = window.location.pathname.replace('/', '');
-const initialRoute = Routes[initialRouteKey.toUpperCase()] || Routes.CONTACT;
+const getRouteKeyFromPath = (path) => path ? path.replace('/', '').toUpperCase() : '';
+const getRouteFromKey = (key) => Routes[key] || Routes.CONTACT;
+const getRouteFromPath = (path) => getRouteFromKey(getRouteKeyFromPath(path));
+const initialRouteKey = getRouteKeyFromPath(window.location.pathname);
+const initialRoute = getRouteFromKey(initialRouteKey);
 
 export const RouterModule = {
   state: {
     route: initialRoute
   },
   actions: {
-    setRoute(state, actions, newRoute, options) {
-      const historyState = { route: newRoute, options };
-      if (options && options.replace) {
-        window.history.replaceState(historyState, null, newRoute.path);
-      }
-      else {
-        window.history.pushState(historyState, null, newRoute.path);
-      }
-
-      if (newRoute.title) {
-        document.title = `Vladimir Sabev - ${newRoute.title}`;
+    setRoute(state, actions, { route, options }) {
+      if (!(options && options.skipHistoryStateUpdate)) {
+        const routeKey = getRouteKeyFromPath(route.path);
+        if (options && options.replace) {
+          window.history.replaceState(routeKey, null, route.path);
+        }
+        else {
+          window.history.pushState(routeKey, null, route.path);
+        }
       }
 
-      return { route: newRoute };
+      if (route.title) {
+        document.title = `Vladimir Sabev - ${route.title}`;
+      }
+
+      return { route: route };
     }
   }
 };
 
-// TODO: Move inside `app({ init })` function when the next version of Hyperapp is released
+// TODO: Move inside `app({ init })` function when the next version of Hyperapp is released,
+// and use `actions.setRoute` from witin the slice
+window.history.replaceState(initialRouteKey, null, initialRoute.path);
 window.addEventListener('popstate', (e) => {
-  if (e && e.state) {
-    Actions.router.setRoute(e.state.route, { ...e.state.options, replace: true });
-  }
+  Actions.router.setRoute({ route: getRouteFromPath(e && e.state), options: { skipHistoryStateUpdate: true } });
 });
 
 // Both `href` and `onclick` can be overridden by the developer for more flexibility.
@@ -48,7 +53,7 @@ export const Link = ({ route, options, onclick, ...props }, children) =>
   <a href={route.path} {...props} onclick={onclick || setRouteAndReturnFalse(route, options)}>{children}</a>
 ;
 
-const setRouteAndReturnFalse = (...args) => () => {
-  Actions.router.setRoute(...args);
+const setRouteAndReturnFalse = (route, options) => () => {
+  Actions.router.setRoute({ route, options });
   return false; // Cancels the default route change so we can get a nice SPA :)
 };

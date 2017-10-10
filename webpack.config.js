ffa3e185e04c066e385e19ab92d4b1be724caf96
Vladimir Sabev <vdsabev@gmail.com>
2017-10-10 13:08:02 +0300

    Added webpack plugin to generate manifest.json

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,7 @@ const CleanWebpackPlugin = require('clean-webpack-plugin')
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const NullPlugin = require('webpack-null-plugin');
+const PwaManifestPlugin = require('webpack-pwa-manifest');
 const WorkboxPlugin = require('workbox-webpack-plugin');
 
 const BUILD_DIR = './build';
@@ -45,6 +46,22 @@ module.exports = ({ production } = {}) => ({
     // `contenthash` is specific to this plugin, we would typically use `chunkhash`
     new ExtractTextPlugin('style.[contenthash].css'),
     new HtmlWebpackPlugin({ template: `./${APP_DIR}/index.ejs`, style }),
+    new PwaManifestPlugin({
+      name: 'Vladimir Sabev',
+      short_name: 'vdsabev',
+      background_color: style.css.neutralLighter,
+      theme_color: style.css.primary,
+      start_url: '/',
+      display: 'standalone',
+      orientation: 'portrait',
+      icons: [{
+        src: path.resolve(APP_DIR, 'avatar.png'),
+        sizes: [48, 96, 128, 192, 256, 384, 512]
+      }],
+
+      // Custom options
+      publicPath: undefined
+    }),
     new WorkboxPlugin({
       globDirectory: BUILD_DIR,
       globPatterns: ['**/*.{html,js,css}'],

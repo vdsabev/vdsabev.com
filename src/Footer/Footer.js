4d4ec1439b8af87410530d5c710bdae65076b09a
Vladimir Sabev <vdsabev@gmail.com>
2017-10-11 17:48:24 +0300

    Changed @spovv attribution link to Behance

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -11,7 +11,7 @@ export const Footer = () => [
   <footer class="footer">
     <div class="footer-content narrow">
       <p>&copy; {thisYear} Vladimir Sabev</p>
-      <p>Illustration by <a target="_blank" href="https://twitter.com/spovv">@spovv</a></p>
+      <p>Illustration by <a target="_blank" href="https://www.behance.net/s-pov">@spovv</a></p>
     </div>
   </footer>
 ];

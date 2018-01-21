4a7fdca9416a519cb9b3061b630092696a05f47c
Vladimir Sabev <vdsabev@gmail.com>
2018-01-21 04:17:02 +0200

    Upgraded to newer derpy syntax for app

--- src/App/index.ts
+++ src/App/index.ts
@@ -1,14 +1,15 @@
+import { createStore } from 'derpy';
 import { app } from 'derpy/app/picodom';
+import { debug } from 'derpy/debug/redux-devtools';
+
 import { patch } from '../dom';
 
 import { AppModel } from './AppModel';
 import { App } from './App';
 
-const store = app({
-  patch,
-  model: new AppModel(),
-  view: App,
-});
+const model = new AppModel();
+const store = process.env.NODE_ENV === 'production' ? createStore(model) : debug(createStore(model));
+app({ store, view: App, patch });
 
 // We need to use `setTimeout` for the animation to run properly
 setTimeout(store.model.animate, 0);

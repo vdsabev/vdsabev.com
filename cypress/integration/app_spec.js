7d94225d4406afefa08c209fd75300fcda4a36bc
Vladimir Sabev <vdsabev@gmail.com>
2018-03-24 00:17:27 +0200

    Extracted test settings to config file

--- cypress/integration/app_spec.js
+++ cypress/integration/app_spec.js
@@ -1,26 +1,16 @@
-const env = {
-  NODE_ENV: 'development',
-  rootUrl: {
-    development: 'http://localhost:3000',
-    production: 'http://vdsabev.com',
-  }
-};
-
 describe(`App`, () => {
-  const rootUrl = env.rootUrl[env.NODE_ENV];
-
   it(`should have the correct title`, () => {
-    cy.visit(rootUrl);
+    cy.visit('/');
     cy.title().should('include', 'Vladimir Sabev');
   });
 
-  it(`should redirect root URL to /contact`, () => {
-    cy.visit(rootUrl);
-    cy.url().should('eq', `${rootUrl}/contact`);
+  it(`should redirect from base URL to /contact`, () => {
+    cy.visit('/');
+    cy.location('pathname').should('eq', '/contact');
   });
 
   it(`should redirect and activate contact link`, () => {
-    cy.visit(rootUrl);
+    cy.visit('/');
     cy.get('a[href="/contact"]').should('have.class', 'active');
   });
 
@@ -32,12 +22,12 @@ describe(`App`, () => {
       { name: 'Talks',   href: '/talks',   selector: '.articles' },
     ].forEach((page) => {
       it(`should have active ${page.name} link`, () => {
-        cy.visit(rootUrl + page.href);
+        cy.visit(page.href);
         cy.get(`a[href="${page.href}"]`).should('have.class', 'active');
       });
 
       it(`should have ${page.name} page`, () => {
-        cy.visit(rootUrl + page.href);
+        cy.visit(page.href);
         cy.get('.page-container').find(page.selector).should('have.length', 1);
       });
     });

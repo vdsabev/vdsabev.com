cc771757136e7cca1540bf901a0af35a26d5db5b
Vladimir Sabev <vdsabev@gmail.com>
2018-10-06 02:39:06 +0300

    Fixed availability spacing

--- src/Contact/Contact.tsx
+++ src/Contact/Contact.tsx
@@ -14,9 +14,9 @@ const getAvailabilityStyle = (status: string) => ({
 export const Contact = ({ model, ...props }: { model: ContactModel }) => (
   <section class="contact narrow spacer" {...props}>
     <p>
-      I'm currently
-      <b style={getAvailabilityStyle(model.availability.status)}>{model.availability.status}</b>
-      for projects and consulting for <b>{model.availability.range}</b>.
+      I'm currently{' '}
+      <b style={getAvailabilityStyle(model.availability.status)}>{model.availability.status}</b> for
+      projects and consulting for <b>{model.availability.range}</b>.
     </p>
 
     <form name="contact" oncreate={model.setFormAction} onsubmit="return false">

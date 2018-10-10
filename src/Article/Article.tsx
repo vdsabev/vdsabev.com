8f6a641877e52d5f9e08157ace49f538784992cb
Vladimir Sabev <vdsabev@gmail.com>
2018-10-10 16:47:22 +0300

    Added preview image to articles

--- src/Article/Article.tsx
+++ src/Article/Article.tsx
@@ -13,14 +13,27 @@ export const Articles = ({ articles, ...props }: ArticlesProps) => (
   </section>
 );
 
+const getPreviewUrl = (url: string) =>
+  `${process.env.PREVIEW_SERVICE_URL}?url=${encodeURIComponent(
+    url
+  )}&deviceScaleFactor=0.25&type=jpeg&quality=50`;
+
 const Article = (article: Article) => (
   <article key={article.url} class="article">
-    <a target="_blank" rel="noopener" href={article.url} class="article__title">
-      {article.title}
+    <a target="_blank" rel="noopener" href={article.url}>
+      <img class="article__img" src={getPreviewUrl(article.url)} />
     </a>
-    <p class="article__text">{article.description}</p>
-    <time dateTime={article.date} class="article__date">
-      {new Date(article.date).toDateString()}
-    </time>
+
+    <div>
+      <a target="_blank" rel="noopener" href={article.url} class="article__title">
+        {article.title}
+      </a>
+
+      <p class="article__text">{article.description}</p>
+
+      <time dateTime={article.date} class="article__date">
+        {new Date(article.date).toDateString()}
+      </time>
+    </div>
   </article>
 );
